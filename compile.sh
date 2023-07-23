224169060c72eb4c1dcdc71039bd4424881cc035
doneforaiur <mirza.a.96@hotmail.com>
2023-07-23 16:13:27 +0300

    sed dynamic insert

--- compile.sh
+++ compile.sh
@@ -27,10 +27,11 @@ for file in $(ls -t ./entries/current/*.md); do
 
     title_of_entry=$(echo "$basefilename" | sed 's/\..*$//;s/_/ /g')
 
-    echo "$title_of_entry"
     echo "<tr><td style=\"text-align: left;\">$title_of_entry</td></tr>" >> ./tmp/current_table.html
 
-    sed -i "18r ./tmp/current_table.html" ./tmp/current.html
+    insert_line=$(( $(grep -n "auto-generate-marker" ./tmp/current.html | cut -f1 -d: | head -1)))
+
+    sed -i -e "${insert_line}r ./tmp/current_table.html" "./tmp/current.html"
     rm ./tmp/current_table.html
 done
 
@@ -39,21 +40,26 @@ for file in $(ls -t ./entries/standalone/*.md); do
     basefilename="$(basename  $file)"
     basefilename=$(echo "$basefilename" | sed 's/\..*$//')
 
-    echo "$basefilename"
-    pandoc -f markdown -t html "$file" > ./tmp/bare_"${basefilename}".html
-    cp ./templates/header.html ./www/"${basefilename}".html
-    sed -i "50r ./tmp/bare_"${basefilename}".html" ./www/"${basefilename}".html
+    pandoc -f markdown -t html "$file" > "./tmp/bare_${basefilename}.html"
+    cp "./templates/header.html" "./www/${basefilename}.html"
+
+    insert_line=$(( $(grep -n "auto-generate-marker" ./www/"${basefilename}".html | cut -f1 -d: | head -1) ))
+
+    sed -i -e "${insert_line}r ./tmp/bare_${basefilename}.html" ./www/${basefilename}.html 
 done
 
 # recent and archive entries
-for file in $(ls -t ./entries/recent/*.md); do
+for file in $(ls -tr ./entries/recent/*.md); do
     basefilename="$(basename  $file)"
 
     title_of_entry=$(echo "$basefilename" | sed 's/\..*$//;s/_/ /g')
 
     pandoc -f markdown -t html "$file" > ./tmp/bare_"${basefilename%.*}".html
     cp ./templates/header.html ./www/entries/"${basefilename%.*}".html
-    sed -i "50r ./tmp/bare_"${basefilename%.*}".html" ./www/entries/"${basefilename%.*}".html
+
+    insert_line=$(( $(grep -n "auto-generate-marker" ./www/entries/"${basefilename%.*}".html | cut -f1 -d: | head -1) ))
+    sed -i -e "${insert_line}r ./tmp/bare_${basefilename%.*}.html" "./www/entries/${basefilename%.*}.html"
+
     
     created=$(stat -c "%y" "$file")
     created=$(date -d "$created" +"%d/%m/%Y")
@@ -61,7 +67,9 @@ for file in $(ls -t ./entries/recent/*.md); do
     echo "<tr><td style=\"text-align: left;\">$created</td>" >> ./tmp/archive_table.html
     echo "<td style=\"text-align: left;\"><a href=\"entries/${basefilename%.*}.html\">$title_of_entry</a></td></tr>" >> ./tmp/archive_table.html
 
-    sed -i "16r ./tmp/archive_table.html" ./tmp/archive.html
+    insert_line=$(( $(grep -n "auto-generate-marker" ./tmp/archive.html | cut -f1 -d: | head -1) ))
+
+    sed -i "${insert_line}r ./tmp/archive_table.html" ./tmp/archive.html
     rm ./tmp/archive_table.html
 
 
@@ -72,7 +80,9 @@ for file in $(ls -t ./entries/recent/*.md); do
     echo "<tr><td style=\"text-align: left;\">$created</td>" >> ./tmp/recent_table.html
     echo "<td style=\"text-align: left;\"><a href=\"entries/${basefilename%.*}.html\">$title_of_entry</a></td></tr>" >> ./tmp/recent_table.html
 
-    sed -i "16r ./tmp/recent_table.html" ./tmp/recent.html
+    insert_line=$(( $(grep -n "auto-generate-marker" ./tmp/recent.html | cut -f1 -d: | head -1) ))
+    sed -i "${insert_line}r ./tmp/recent_table.html" "./tmp/recent.html"
+
     rm ./tmp/recent_table.html
 done
 
@@ -86,14 +96,18 @@ for file in $(ls -t ./entries/soon/*.md); do
     echo "<tr><td style=\"text-align: left;\">$created</td>" >> ./tmp/soon_table.html
     echo "<td style=\"text-align: left;\">$title_of_entry</td></tr>" >> ./tmp/soon_table.html
 
-    sed -i "19r ./tmp/soon_table.html" ./tmp/soon.html
+    insert_line=$(($(grep -n "auto-generate-marker" ./tmp/soon.html | cut -f1 -d: | head -1 )))
+    sed -i "${insert_line}r ./tmp/soon_table.html" ./tmp/soon.html
+
     rm ./tmp/soon_table.html
 done
 
 cat ./tmp/recent.html ./tmp/current.html ./tmp/soon.html > ./tmp/index_body.html
-sed -i "50r ./tmp/index_body.html" ./www/index.html
-sed -i "50r ./tmp/archive.html" ./www/archive.html
+insert_line=$(($(grep -n "auto-generate-marker" ./tmp/soon.html | cut -f1 -d: | head -1 )))
+
+sed -i "${insert_line}r ./tmp/index_body.html" ./www/index.html
+sed -i "${insert_line}r ./tmp/archive.html" ./www/archive.html
 pandoc -f markdown -t html ./templates/about.md > ./tmp/about_body.html
-sed -i "50r ./tmp/about_body.html" ./www/about.html
+sed -i "${insert_line}r ./tmp/about_body.html" ./www/about.html
 
 rm -rf ./tmp
